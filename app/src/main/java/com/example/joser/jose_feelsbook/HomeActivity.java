052e33cf20a1dde3f0663e60b1d2a4d0be693c0b
Jose Ramirez <jose@ualberta.ca>
2018-10-01 10:16:09 -0600

    renamed the Home activity to HomeActivity for better readability

--- app/src/main/java/com/example/joser/jose_feelsbook/HomeActivity.java
+++ app/src/main/java/com/example/joser/jose_feelsbook/HomeActivity.java
@@ -3,7 +3,7 @@ package com.example.joser.jose_feelsbook;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Home extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
